4656c7bc74f0423a0b3438aad21d10b33ff6eb93
sauljavier <psauljavier6@gmail.com>
2025-04-12 00:43:57 -0700

    prueba de parametros estaticos en typeproduct

--- src/components/products/productscategory/ProductsCategory.tsx
+++ src/components/products/productscategory/ProductsCategory.tsx
@@ -1,6 +1,5 @@
-"use client";
-
 import { useEffect, useState } from "react";
+import axios from "axios"; // Importa Axios
 import { useTranslation } from "react-i18next";
 import ProductCard from "@/components/productcard/productcard";
 
@@ -23,43 +22,43 @@ export default function ProductsCategory({ category }: ProductsCategoryProps) {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_URL}/api/products/category/${category}`)
-      .then((res) => res.json())
-      .then((data) => setProducts(data))
+    // Usamos axios para hacer la petición
+    axios
+      .get(`${process.env.NEXT_PUBLIC_URL}/api/products/category/${category}`)
+      .then((response) => {
+        setProducts(response.data); // Axios devuelve los datos en response.data
+      })
       .catch((error) => {
         setError(t("errorFetchingProducts"));
       })
       .finally(() => {
         setLoading(false);
       });
-  }, [category,t]);
+  }, [category, t]);
 
   return (
-      <div className="container mx-auto p-2">
-        <h1 className="text-3xl font-bold mb-4 text-center">
-        {t("productsTitle")}
-        </h1>
-  
-        {loading && (
-          <p className="text-gray-500 col-span-full text-center">Loading products...</p>
-        )}
-  
-        {error && (
-          <p className="text-red-500 col-span-full text-center">{error}</p>
-        )}
-  
-        {!loading && !error && products.length === 0 && (
-          <p className="text-gray-500 col-span-full text-center">
-            There are no products available.
-          </p>
-        )}
-  
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {products.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
-        </div>
+    <div className="container mx-auto p-2">
+      <h1 className="text-3xl font-bold mb-4 text-center">{t("productsTitle")}</h1>
+
+      {loading && (
+        <p className="text-gray-500 col-span-full text-center">Loading products...</p>
+      )}
+
+      {error && (
+        <p className="text-red-500 col-span-full text-center">{error}</p>
+      )}
+
+      {!loading && !error && products.length === 0 && (
+        <p className="text-gray-500 col-span-full text-center">
+          {t("noProducts")}
+        </p>
+      )}
+
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
