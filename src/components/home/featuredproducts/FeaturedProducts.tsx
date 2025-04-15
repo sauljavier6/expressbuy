e969338165fab704fafb899ad897fa599b6bfacf
sauljavier <psauljavier6@gmail.com>
2025-04-14 19:51:48 -0700

    correccion de guardado de imagenes

--- src/components/home/featuredproducts/FeaturedProducts.tsx
+++ src/components/home/featuredproducts/FeaturedProducts.tsx
@@ -53,13 +53,7 @@ const FeaturedProducts = () => {
           {products.map((product) => (
             <div key={product._id} className="border rounded-lg p-4 shadow-md bg-white relative">
               <div className="relative">
-                <Image 
-                  src={product.image} 
-                  alt={product.name} 
-                  width={200} 
-                  height={200} 
-                  className="w-full h-48 object-cover rounded-md"
-                />
+                <img src={product.image} alt={product.name} width={200} height={200} className="w-full h-48 object-cover rounded-md"/>
               </div>
               <p className="text-xs text-gray-500 mt-2">{product.category}</p>
               <h3 className="text-sm font-semibold">{product.name}</h3>
